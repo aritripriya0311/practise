b6e58304079765fccfada5ef1654b21bca5c7a8f
Aritri Priya Saha <67187980+aritripriya0311@users.noreply.github.com>
2022-11-23 18:15:28 +0600

    search changes 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    Ng2SearchPipeModule,
     FormsModule
   ],
   providers: [],
