107728c7d91dfcac20f7d1ff0c57d123772d350b
Aritri Priya Saha <67187980+aritripriya0311@users.noreply.github.com>
2022-11-23 12:39:04 +0600

    Change Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,8 @@ export class AppComponent implements OnInit {
 
   title = 'launchPad';
   flag: string[] = ['false', 'false', 'false', 'false', 'false', 'false', 'false', 'false', 'false', 'false'];
+  searchArray: any[] = [];
+  subscribeApi: any;
 
   ngOnInit(): void {
     for (let i = 0; i < 10; i++) {
@@ -27,5 +30,89 @@ export class AppComponent implements OnInit {
       this.flag[i] = 'true'
     }
   }
+
+//   search() {
+//     let input = document.getElementById('commandbar-input-search')
+//     input=input?.innerHTML.toLowerCase() ;
+//     let x = document.getElementsByClassName('list-group-item');
+      
+//     for (let i = 0; i < x.length; i++) { 
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display="none";
+//         }
+//         else {
+//             x[i].style.display="list-item";                 
+//         }
+//     }
+//  }
+
+// search(){
+// const searchInput = document.getElementById("commandbar-input-search");
+// const namesFromDOM: HTMLCollectionOf<HTMLLIElement> = document.getElementsByTagName('li');
+
+// searchInput?.addEventListener("keyup", (event) => {
+//   const  value  = event.target;
+  
+//   // get user search input converted to lowercase
+//   const searchQuery = value;
+//   console.log("test7",searchQuery)
+  
+//   // for (const nameElement of namesFromDOM) {
+//   //     // store name text and convert to lowercase
+//   //     let name = nameElement.textContent.toLowerCase();
+      
+//   //     // compare current name to search input
+//   //     if (name.includes(searchQuery)) {
+//   //         // found name matching search, display it
+//   //         nameElement.style.display = "block";
+//   //     } else {
+//   //         // no match, don't display name
+//   //         nameElement.style.display = "none";
+//   //     }
+//   // }
+// }
+// )
+
+// }
+// };
+
+
+// unSubApiCall = () => {
+//   if (this.subscribeApi != undefined) {
+//     this.subscribeApi.unsubscribe();
+    
+//   }
+// };
+
+// search = (evt: any) => {
+  
+//   let input = (<HTMLInputElement>document.getElementById('commandbar-input-search'));
+//   let filter = input?.value.toUpperCase();
+//   //let ul = document.getElementById("myUL")?.innerText;
+//   let li = document.getElementsByTagName('li') ;
+
+//   for (let i = 0; i < li.length; i++) {
+    
+//     const a = li[i].getElementsByTagName("a")[0];
+    
+//     let txtValue = a?.textContent || a?.innerText;
+//     if (txtValue?.toUpperCase().indexOf(filter) > -1) {
+//       li[i].style.display = "";
+//     } else {
+//       li[i].style.display = "none";
+//     }
+//     //console.log("test 8", a)
+//   }
+  
+
+//       // console.log("test2", ul);
+//       // console.log("test3", filter);
+    
+      
+//        console.log("test4", li)
+    
+// }
+
+
 
 }
