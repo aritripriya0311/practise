55c745bab0865e8931c8150e392afa86d933cee4
Aritri Priya Saha <67187980+aritripriya0311@users.noreply.github.com>
2022-11-24 10:25:58 +0600

    Icons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,55 +22,55 @@ export class AppComponent implements OnInit {
     //tab1
     [
       {
-        title: 'Import contacts', iconName: 'near_me'
+        title: 'Import contacts', iconName: 'account_circle'
       },
       {
-        title: 'Connect your email', iconName: 'near_me'
+        title: 'Connect your email', iconName: 'email'
       },
       {
-        title: 'Add your first deal', iconName: 'near_me'
+        title: 'Add your first deal', iconName: 'currency_exchange'
       },
       {
-        title: ' Send your first email', iconName: 'near_me'
+        title: ' Send your first email', iconName: 'forward_to_inbox'
       },
       {
-        title: 'Automate processes using workflow', iconName: 'near_me'
+        title: 'Automate processes using workflow', iconName: 'lan'
       },
       {
-        title: 'Invite your team', iconName: 'near_me'
+        title: 'Invite your team', iconName: 'person_outline'
       },
     ],
     //tab2
     [
       {
-        title: 'Accounting and invoicing', iconName: 'near_me'
+        title: 'Accounting and invoicing', iconName: 'receipt_long'
       },
       {
         title: 'Data sync', iconName: 'near_me'
       },
       {
-        title: 'Files and proposals', iconName: 'near_me'
+        title: 'Files and proposals', iconName: 'folder'
       },
       {
-        title: 'Collaboration', iconName: 'near_me'
+        title: 'Collaboration', iconName: 'group_add'
       },
       {
-        title: 'Customer support', iconName: 'near_me'
+        title: 'Customer support', iconName: 'face'
       },
       {
-        title: 'Contacts,email, and calendar', iconName: 'near_me'
+        title: 'Contacts,email, and calendar', iconName: 'account_circle'
       },
       {
-        title: 'Lead generation', iconName: 'near_me'
+        title: 'Lead generation', iconName: 'account_box'
       },
       {
-        title: 'More apps', iconName: 'near_me'
+        title: 'More apps', iconName: 'extension'
       },
     ],
     //tab3
     [
       {
-        title: 'Import contacts', iconName: 'near_me'
+        title: 'Import contacts', iconName: 'account_circle'
       },
       {
         title: 'Create sales sequence', iconName: 'near_me'
@@ -79,49 +79,49 @@ export class AppComponent implements OnInit {
         title: 'Build your deal pipeline', iconName: 'near_me'
       },
       {
-        title: 'Create workflow', iconName: 'near_me'
+        title: 'Create workflow', iconName: 'lan'
       },
       {
-        title: 'Invite your team', iconName: 'near_me'
+        title: 'Invite your team', iconName: 'people'
       },
       {
-        title: 'Connect your email and calendar', iconName: 'near_me'
+        title: 'Connect your email and calendar', iconName: 'email'
       },
     ],
     //tab4
     [
       {
-        title: 'Integrate webforms', iconName: 'near_me'
+        title: 'Integrate webforms', iconName: 'article'
       },
       {
-        title: 'Track your website', iconName: 'near_me'
+        title: 'Track your website', iconName: 'work'
       },
       {
-        title: 'Import contacts', iconName: 'near_me'
+        title: 'Import contacts', iconName: 'account_circle'
       },
       {
-        title: 'Connect your email and calendar', iconName: 'near_me'
+        title: 'Connect your email and calendar', iconName: 'email'
       }
     ],
     //tab5
     [
       {
-        title: 'Send Mail', iconName: 'near_me'
+        title: 'Send email', iconName: 'forward_to_inbox'
       },
       {
         title: 'Create sales sequence', iconName: 'near_me'
       },
       {
-        title: 'Score your contatcs', iconName: 'near_me'
+        title: 'Score your contacts', iconName: 'near_me'
       },
       {
-        title: 'Set up phone', iconName: 'near_me'
+        title: 'Set up phone', iconName: 'wifi_calling'
       },
       {
-        title: 'Send SMS', iconName: 'near_me'
+        title: 'Send SMS', iconName: 'send_to_mobile'
       },
       {
-        title: 'Create sales activity', iconName: 'near_me'
+        title: 'Create sales activity', iconName: 'content_paste'
       },
     ],
     //tab6
@@ -133,22 +133,22 @@ export class AppComponent implements OnInit {
         title: 'Create sales quotes', iconName: 'near_me'
       },
       {
-        title: 'Set quotas for your team', iconName: 'near_me'
+        title: 'Set quotas for your team', iconName: 'trending_up'
       }
     ],
     //tab7
     [
       {
-        title: 'View sales dashboard', iconName: 'near_me'
+        title: 'View sales dashboard', iconName: 'timeline'
       },
       {
-        title: 'Create sales report', iconName: 'near_me'
+        title: 'Create sales report', iconName: 'leaderboard'
       }
     ],
     //tab8
     [
       {
-        title: 'Create work flow', iconName: 'near_me'
+        title: 'Create work flow', iconName: 'play_circle'
       },
       {
         title: 'Create auto-assignment rule', iconName: 'near_me'
@@ -157,7 +157,7 @@ export class AppComponent implements OnInit {
     //tab9
     [
       {
-        title: 'Invite your team', iconName: 'near_me'
+        title: 'Invite your team', iconName: 'group'
       },
       {
         title: 'Manage roles and permissions', iconName: 'near_me'
@@ -182,87 +182,6 @@ export class AppComponent implements OnInit {
 
   }
 
-//   search() {
-//     let input = document.getElementById('commandbar-input-search')
-//     input=input?.innerHTML.toLowerCase() ;
-//     let x = document.getElementsByClassName('list-group-item');
-      
-//     for (let i = 0; i < x.length; i++) { 
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//             x[i].style.display="none";
-//         }
-//         else {
-//             x[i].style.display="list-item";                 
-//         }
-//     }
-//  }
-
-// search(){
-// const searchInput = document.getElementById("commandbar-input-search");
-// const namesFromDOM: HTMLCollectionOf<HTMLLIElement> = document.getElementsByTagName('li');
-
-// searchInput?.addEventListener("keyup", (event) => {
-//   const  value  = event.target;
-  
-//   // get user search input converted to lowercase
-//   const searchQuery = value;
-//   console.log("test7",searchQuery)
-  
-//   // for (const nameElement of namesFromDOM) {
-//   //     // store name text and convert to lowercase
-//   //     let name = nameElement.textContent.toLowerCase();
-      
-//   //     // compare current name to search input
-//   //     if (name.includes(searchQuery)) {
-//   //         // found name matching search, display it
-//   //         nameElement.style.display = "block";
-//   //     } else {
-//   //         // no match, don't display name
-//   //         nameElement.style.display = "none";
-//   //     }
-//   // }
-// }
-// )
-
-// }
-// };
-
-
-// unSubApiCall = () => {
-//   if (this.subscribeApi != undefined) {
-//     this.subscribeApi.unsubscribe();
-    
-//   }
-// };
-
-// search = (evt: any) => {
-  
-//   let input = (<HTMLInputElement>document.getElementById('commandbar-input-search'));
-//   let filter = input?.value.toUpperCase();
-//   //let ul = document.getElementById("myUL")?.innerText;
-//   let li = document.getElementsByTagName('li') ;
-
-//   for (let i = 0; i < li.length; i++) {
-    
-//     const a = li[i].getElementsByTagName("a")[0];
-    
-//     let txtValue = a?.textContent || a?.innerText;
-//     if (txtValue?.toUpperCase().indexOf(filter) > -1) {
-//       li[i].style.display = "";
-//     } else {
-//       li[i].style.display = "none";
-//     }
-//     //console.log("test 8", a)
-//   }
-  
-
-//       // console.log("test2", ul);
-//       // console.log("test3", filter);
-    
-      
-//        console.log("test4", li)
-    
-// }
 
 
 
